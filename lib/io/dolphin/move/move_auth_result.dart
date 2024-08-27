d7cc0f89450aac3cc3a105835361763411d30aa1
Clemens Unterkofler <clemens.unterkofler@dolph.in>
2024-08-27 16:25:41 +0200

    add move auth result file

---
+++ lib/io/dolphin/move/move_auth_result.dart
@@ -0,0 +1,24 @@
+/// An error status used in `MoveAuthResult`
+enum AuthSetupStatus {
+  success,
+
+  /// A network error occurred.
+  networkError,
+
+  /// Auth code is invalid.
+  invalidCode,
+}
+
+/// An error returned by `setup(String authCode, ...)`
+class MoveAuthResult {
+  /// Authentication status.
+  AuthSetupStatus status;
+
+  /// Status message.
+  String description;
+
+  MoveAuthResult(
+    this.status,
+    this.description,
+  );
+}
