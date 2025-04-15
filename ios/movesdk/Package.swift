7d0906b8a0b930ca4881254cafbae0bffa19a206
Clemens Unterkofler <clemens.unterkofler@dolph.in>
2025-04-15 09:46:58 +0200

    upgrade to 2.12.0, enable swift package manager.

---
+++ ios/movesdk/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "movesdk",
+    platforms: [
+        .iOS("13.0"),
+    ],
+    products: [
+        .library(name: "movesdk", targets: ["movesdk"])
+    ],
+    dependencies: [
+		.package(url: "https://github.com/dolphin-technologies/MOVE-iOS-Packages", from: "2.12.0")
+	],
+    targets: [
+        .target(
+            name: "movesdk",
+            dependencies: [
+				.product(name: "DolphinMoveSDK", package: "move-ios-packages")
+			],
+            resources: [
+                .process("PrivacyInfo.xcprivacy"),
+            ]
+        )
+    ]
+)
